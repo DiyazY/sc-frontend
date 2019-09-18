4a1d3f5e175173d91b8fee4cb2c33f927ad58961
Diyaz Yakubov <diyaz.yakubov@exsensio.com>
2019-09-18 23:12:49 +0300

    prettify and simplify

--- src/hooks/useMeasurements.js
+++ src/hooks/useMeasurements.js
@@ -1,36 +1,39 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { initSignalRService, getSignalRService } from '../services/signalRService';
+import {
+  initSignalRService,
+  getSignalRService
+} from "../services/signalRService";
 
-const useMeasurements = (url) => {
+const useMeasurements = url => {
+  const [cpu, setCpu] = useState(0);
+  const [ram, setRam] = useState(0);
 
-    const [cpu, setCpu] = useState(0);
-    const [ram, setRam] = useState(0);
+  useEffect(() => {
+    if (!url) return;
 
-    useEffect(() => {
-        if (!url) return;
+    const subscriptionName = "stream";
+    initSignalRService(url);
+    let hub = undefined;
 
+    hub = getSignalRService();
+    if (!hub) return;
 
-        const subscriptionName = 'stream';
-        initSignalRService(url);
-        let hub = undefined;
-    
-        hub = getSignalRService();
-        if (!hub) return;
-        
-        hub.subscribeToOnStream(subscriptionName, (channel, payload) => {
-            if(payload.name === 'CPU'){
-                setCpu(payload.percentage);
-            }else if(payload.name === 'RAM'){
-                setRam(payload.percentage);
-            }
-        });
+    hub.subscribeToOnStream(subscriptionName, (channel, payload) => {
+      if (payload.name === "CPU") {
+        setCpu(payload.percentage);
+      } else if (payload.name === "RAM") {
+        setRam(payload.percentage);
+      }
+    });
 
-        return () => {
-            hub && hub.unsubcribeFromOnStream(subscriptionName);
-        }
-    }, [url]);
-    return {cpu, ram};
-}
+    return () => {
+      hub && hub.unsubcribeFromOnStream(subscriptionName);
+    };
+  }, [url]);
+  return { cpu, ram };
+};
 
 export default useMeasurements;
+
+
--- src/services/signalRService.js
+++ src/services/signalRService.js
@@ -1,54 +1,33 @@
-const signalR = require('@aspnet/signalr');
+const signalR = require("@aspnet/signalr");
 
 let signalRService = undefined;
 
-export function initSignalRService(hubUrl = '') {
-    try {
+export function initSignalRService(hubUrl = "") {
+  try {
+    let hub = new signalR.HubConnectionBuilder().withUrl(hubUrl).build();
 
-        let hub = new signalR.HubConnectionBuilder()
-            .withUrl(hubUrl)
-            .build();
+    const onStreamSubscribers = new Map(); //contains all subscribers of onStream
 
-        const restartHub = async () => {
-            try {
-                await hub.start();
-                console.assert(hub.state === signalR.HubConnectionState.Connected);
-                console.log("connected");
-            } catch (err) {
-                console.assert(hub.state === signalR.HubConnectionState.Disconnected);
-                console.error('restart hub', err);
-                setTimeout(() => restartHub(), 5000);
-            }
-        };
+    hub.on("onStream", payload => {
+      onStreamSubscribers.forEach(p => p.call(null, "stream", payload));
+    });
 
-        hub.onclose(() => {
-            console.assert(hub.state === signalR.HubConnectionState.Disconnected);
-            console.log('connecition closed');
-            restartHub();
-        });
+    hub.start();
 
-        const onStreamSubscribers = new Map();//contains all subscribers of onStream
-
-        hub.on(
-            'onStream',
-            (payload) => {
-                onStreamSubscribers.forEach(p => p.call(null,"stream",  payload));}
-        );
-
-        hub.start();
-
-        signalRService = {
-            getHub: () => hub,
-            subscribeToOnStream: (name, fn) => !onStreamSubscribers.has(name) && onStreamSubscribers.set(name, fn),
-            unsubcribeFromOnStream: (name) => onStreamSubscribers.has(name) && onStreamSubscribers.delete(name)
-        };
-        return true;
-    } catch (error) {
-        console.error('signalR initialization', error);
-        return false;
-    }
+    signalRService = {
+      getHub: () => hub,
+      subscribeToOnStream: (name, fn) =>
+        !onStreamSubscribers.has(name) && onStreamSubscribers.set(name, fn),
+      unsubcribeFromOnStream: name =>
+        onStreamSubscribers.has(name) && onStreamSubscribers.delete(name)
+    };
+    return true;
+  } catch (error) {
+    console.error("signalR initialization", error);
+    return false;
+  }
 }
 
 export function getSignalRService() {
-    return signalRService;
+  return signalRService;
 }
